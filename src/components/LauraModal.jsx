27a36139c3b6bdfba9473d71a83c1e16e0937a7c
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2022-03-14 00:01:36 -0500

    Added more stuffs.

--- src/components/LauraModal.jsx
+++ src/components/LauraModal.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import styles from './LauraModal.module.css';
+import LauraModalHeader from './LauraModalHeader';
 
 function LauraModal() {
     return (
         <div className={styles.overlay}>
             <div className={styles.lauraModal}>
-                <h3>Laura Trebe is a <span style={{ color: 'rgb(10, 165, 10)' }}>PROGRAMMER</span></h3>
+                <LauraModalHeader />
                 <h4 style={{ textAlign: 'center' }}>
-                    She really is,<br/>
+                    She really is,
+                    <br />
                     <span style={{ color: 'rgb(87, 179, 255)' }}>just ask this guy.</span>
-
                 </h4>
-                
-                <img className={styles.image} src='https://www.ecowatch.com/wp-content/uploads/2021/10/646441070-origin.jpg' alt='Laura Trebe Programs Hard' />
+                <a
+                    className={styles.turtleLink}
+                    href="https://www.youtube.com/watch?v=ZH63T005WTE"
+                    rel='noreferrer'
+                    target='_blank'
+                >
+                    <img
+                        className={styles.image}
+                        src="https://www.ecowatch.com/wp-content/uploads/2021/10/646441070-origin.jpg"
+                        alt="Laura Trebe Programs Hard"
+                    />
+                </a>
             </div>
         </div>
     );
--- src/components/ModalCloseIcon.jsx
+++ src/components/ModalCloseIcon.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import * as ModalActions from '../store/actions/modal';
-import { useGameContext } from '../store/GameState';
 
-function ModalCloseIcon({ className }) {
-    const [state, dispatch] = useGameContext();
-
-    const onClick = () => dispatch(ModalActions.toggleShowStatsModal(false));
-
+function ModalCloseIcon({ className, onClick }) {
     return (
         <button onClick={onClick} className={className}>
             X
